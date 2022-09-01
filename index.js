38bb07e56f0928da374b8a52438a0ba0a7649ed4
flyingjjang051 <flyjjang051@gmail.com>
2022-09-01 11:32:28 +0900

    inset voca

--- index.js
+++ index.js
@@ -39,9 +39,46 @@ app.post("/day/add", (req, res) => {
       });
     });
   });
-  //res.json({ test: "ok" });
 });
 
+app.post("/voca/add", (req, res) => {
+  db.collection("counter").findOne({ name: "count" }, (err, result) => {
+    const insertData = {
+      day: req.body.day,
+      eng: req.body.eng,
+      kor: req.body.kor,
+      isDone: req.body.isDone,
+      id: result.vocasTotal,
+    };
+    db.collection("vocas").insertOne(insertData, (err, result) => {
+      db.collection("counter").updateOne({ name: "count" }, { $inc: { vocasTotal: 1 } }, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        res.json({ insert: "ok" });
+      });
+    });
+  });
+});
+
+app.get("/days", (req, res) => {
+  db.collection("days")
+    .find()
+    .toArray((err, result) => {
+      res.json(result); //   페이지 내가 만들어서 보내주기
+    });
+});
+app.get("/voca/:day", (req, res) => {
+  console.log(req.params.day);
+  const _day = parseInt(req.params.day);
+  // db연결하고 해당되는것의 모든 데이터를 받아서 json으로 리턴하기...
+  db.collection("vocas")
+    .find({ day: _day })
+    .toArray((err, result) => {
+      console.log(result);
+      res.json(result);
+    });
+});
 app.listen(PORT, () => {
   console.log(`${PORT}에서 서버 대기중`);
 });
